59ed433ba278a755113a3cedf02974aa1e722970
stouma915 <sasakitouma@yahoo.ne.jp>
2021-10-28 18:29:09 +0900

    水素水を醸造可能にする

--- src/main/java/net/stouma915/hydrogenmod/HydrogenMod.java
+++ src/main/java/net/stouma915/hydrogenmod/HydrogenMod.java
@@ -1,16 +1,27 @@
 package net.stouma915.hydrogenmod;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.stouma915.hydrogenmod.listener.*;
+import net.stouma915.hydrogenmod.recipe.brewing.HydrogenWaterBrewingRecipe;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Mod(HydrogenMod.MODID)
 public class HydrogenMod {
     public static final String MODID = "hydrogenmod";
 
     public HydrogenMod() {
-        MinecraftForge.EVENT_BUS.register(new LivingEntityUseItemFinishListener());
-        MinecraftForge.EVENT_BUS.register(new LivingUpdateListener());
-        MinecraftForge.EVENT_BUS.register(new PlayerInteractListener());
+        Arrays.asList(
+                new LivingEntityUseItemFinishListener(),
+                new LivingUpdateListener(),
+                new PlayerInteractListener()
+        ).forEach(MinecraftForge.EVENT_BUS::register);
+
+        Collections.singletonList(
+                new HydrogenWaterBrewingRecipe()
+        ).forEach(BrewingRecipeRegistry::addRecipe);
     }
 }
---
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/brewing/HydrogenWaterBrewingRecipe.scala
@@ -0,0 +1,27 @@
+package net.stouma915.hydrogenmod.recipe.brewing
+
+import net.minecraft.world.item.alchemy.{PotionUtils, Potions}
+import net.minecraft.world.item.{ItemStack, Items}
+import net.minecraftforge.common.brewing.IBrewingRecipe
+import net.stouma915.hydrogenmod.item.HydrogenItem
+import net.stouma915.hydrogenmod.potion.HydrogenWaterPotion
+
+class HydrogenWaterBrewingRecipe extends IBrewingRecipe {
+  override def isInput(input: ItemStack): Boolean = {
+    val inputItem = input.getItem
+    (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION) && PotionUtils
+      .getPotion(input) == Potions.WATER
+  }
+
+  override def isIngredient(ingredient: ItemStack): Boolean =
+    ingredient.sameItem(new ItemStack(HydrogenItem.item))
+
+  override def getOutput(input: ItemStack, ingredient: ItemStack): ItemStack =
+    if (isInput(input) && isIngredient(ingredient))
+      PotionUtils.setPotion(
+        new ItemStack(input.getItem),
+        HydrogenWaterPotion.potion
+      )
+    else
+      ItemStack.EMPTY
+}
