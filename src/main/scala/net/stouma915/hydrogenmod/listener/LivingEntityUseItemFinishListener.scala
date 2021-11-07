0762da578055ff8bade8811cbc61a0f629036b73
stouma915 <sasakitouma@yahoo.ne.jp>
2021-11-07 19:03:47 +0900

    Update LivingEntityUseItemFinishListener.scala

--- src/main/scala/net/stouma915/hydrogenmod/listener/LivingEntityUseItemFinishListener.scala
+++ src/main/scala/net/stouma915/hydrogenmod/listener/LivingEntityUseItemFinishListener.scala
@@ -1,6 +1,5 @@
 package net.stouma915.hydrogenmod.listener
 
-import net.minecraft.world.item.ItemStack
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.stouma915.hydrogenmod.damagesource.{
@@ -14,14 +13,17 @@ class LivingEntityUseItemFinishListener {
   def onLivingEntityUseItemFinish(
       event: LivingEntityUseItemEvent.Finish
   ): Unit =
-    if (event.getItem.sameItem(new ItemStack(HydrogenItem.item)))
-      event.getEntityLiving.hurt(
-        HydrogenDamageSource.damageSource,
-        event.getEntityLiving.getHealth
-      )
-    else if (event.getItem.sameItem(new ItemStack(OxygenItem.item)))
-      event.getEntityLiving.hurt(
-        OxygenDamageSource.damageSource,
-        event.getEntityLiving.getHealth
-      )
+    event.getItem.getItem match {
+      case HydrogenItem.item =>
+        event.getEntityLiving.hurt(
+          HydrogenDamageSource.damageSource,
+          event.getEntityLiving.getHealth
+        )
+      case OxygenItem.item =>
+        event.getEntityLiving.hurt(
+          OxygenDamageSource.damageSource,
+          event.getEntityLiving.getHealth
+        )
+      case _ =>
+    }
 }
