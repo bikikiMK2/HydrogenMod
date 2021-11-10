9660cf318e6ad68b3c73cad97e97a2dbb8a3a4ce
stouma915 <sasakitouma@yahoo.ne.jp>
2021-11-10 19:37:38 +0900

    水素のブーツを追加

---
+++ src/main/scala/net/stouma915/hydrogenmod/armor/item/HydrogenBootsArmorItem.scala
@@ -0,0 +1,22 @@
+package net.stouma915.hydrogenmod.armor.item
+
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.{ArmorItem, Item}
+import net.minecraft.world.item.Item.Properties
+import net.stouma915.hydrogenmod.HydrogenMod
+import net.stouma915.hydrogenmod.armor.material.HydrogenArmorMaterial
+import net.stouma915.hydrogenmod.creativetab.HydrogenModTab
+
+object HydrogenBootsArmorItem {
+  private val instance: Item = new HydrogenBootsArmorItem()
+    .setRegistryName(HydrogenMod.MODID, "hydrogen_boots")
+
+  def apply(): Item = instance
+}
+
+sealed class HydrogenBootsArmorItem private ()
+    extends ArmorItem(
+      HydrogenArmorMaterial(),
+      EquipmentSlot.FEET,
+      new Properties().tab(HydrogenModTab())
+    )
--- src/main/scala/net/stouma915/hydrogenmod/item/ItemRegister.scala
+++ src/main/scala/net/stouma915/hydrogenmod/item/ItemRegister.scala
@@ -24,6 +24,7 @@ object ItemRegister {
       ElectrolyzerBlockItem(),
       HydrogenHelmetArmorItem(),
       HydrogenChestplateArmorItem(),
-      HydrogenLeggingsArmorItem()
+      HydrogenLeggingsArmorItem(),
+      HydrogenBootsArmorItem()
     ).foreach(event.getRegistry.register)
 }
