17095fa13f1c16b5a6f6a37522788ffd07dd4f95
stouma915 <sasakitouma@yahoo.ne.jp>
2021-11-23 13:52:06 +0900

    Update ElectrolyzerMenu.scala

--- src/main/scala/net/stouma915/hydrogenmod/gui/menu/ElectrolyzerMenu.scala
+++ src/main/scala/net/stouma915/hydrogenmod/gui/menu/ElectrolyzerMenu.scala
@@ -2,10 +2,9 @@ package net.stouma915.hydrogenmod.gui.menu
 
 import net.minecraft.world.entity.player.{Inventory, Player}
 import net.minecraft.world.inventory.{AbstractContainerMenu, MenuType, Slot}
-import net.minecraft.world.item.ItemStack
 import net.minecraftforge.fmllegacy.network.IContainerFactory
 import net.stouma915.hydrogenmod.HydrogenMod
-import net.stouma915.hydrogenmod.implicits._
+import net.stouma915.hydrogenmod.implicits.*
 
 import java.util.function.Supplier
 
@@ -41,45 +40,6 @@ private[hydrogenmod] class ElectrolyzerMenu(
   }
 
   override def stillValid(p_38874_ : Player): Boolean = true
-
-  override def quickMoveStack(p_38941_ : Player, p_38942_ : Int): ItemStack = {
-    var itemStack = ItemStack.EMPTY
-    val slot = this.slots.get(p_38942_)
-
-    if (slot.nonNull && slot.hasItem) {
-      val itemStack1 = slot.getItem
-      itemStack = itemStack1.copy()
-
-      if (p_38942_ < 0) {
-        if (!this.moveItemStackTo(itemStack1, 0, this.slots.size(), true))
-          return ItemStack.EMPTY
-
-        slot.onQuickCraft(itemStack1, itemStack)
-      } else if (!this.moveItemStackTo(itemStack1, 0, 0, false)) {
-        if (p_38942_ < 0 + 27) {
-          if (
-            !this.moveItemStackTo(itemStack1, 0 + 27, this.slots.size(), true)
-          )
-            return ItemStack.EMPTY
-        } else {
-          if (!this.moveItemStackTo(itemStack1, 0, 0 + 27, false))
-            return ItemStack.EMPTY
-        }
-      }
-
-      if (itemStack1.getCount == 0)
-        slot.set(ItemStack.EMPTY)
-      else
-        slot.setChanged()
-
-      if (itemStack1.getCount == itemStack.getCount)
-        return ItemStack.EMPTY
-
-      slot.onTake(p_38941_, itemStack1)
-    }
-
-    itemStack
-  }
 
   override def get(): Map[Int, Slot] = customSlots
 }
