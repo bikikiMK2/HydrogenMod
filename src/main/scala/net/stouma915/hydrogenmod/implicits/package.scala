dbb06c75405605ae4a5e8864c09314a5a0b22990
stouma915 <sasakitouma@yahoo.ne.jp>
2021-11-28 18:29:37 +0900

    Update package.scala

--- src/main/scala/net/stouma915/hydrogenmod/implicits/package.scala
+++ src/main/scala/net/stouma915/hydrogenmod/implicits/package.scala
@@ -9,38 +9,50 @@ import net.minecraft.world.item.{Item, ItemStack}
 package object implicits {
 
   implicit class AnyRefOps(anyRef: AnyRef) {
+
     def isNull: Boolean = anyRef == null
 
     def nonNull: Boolean = !isNull
+
   }
 
   implicit class ItemOps(item: Item) {
+
     def toGeneralItemStack: ItemStack = new ItemStack(item)
 
     def canDestroy: Boolean = item.isDamageable(toGeneralItemStack)
+
   }
 
   implicit class ItemStackOps(itemStack: ItemStack) {
+
     def destroyItem(livingEntity: LivingEntity): Unit =
       itemStack.hurtAndBreak(
         itemStack.getMaxDamage,
         livingEntity,
         (_: LivingEntity) => {}
       )
+
   }
 
   implicit class EntityOps(entity: Entity) {
+
     def getPos: BlockPos = new BlockPos(entity.getX, entity.getY, entity.getZ)
+
   }
 
   implicit class LivingEntityPos(livingEntity: LivingEntity) {
+
     def killWithCause(cause: DamageSource): Unit =
       livingEntity.hurt(cause, livingEntity.getMaxHealth)
+
   }
 
   implicit class SlotOps(slot: Slot) {
+
     def contains(item: Item): Boolean =
       slot.hasItem && slot.getItem.nonNull && !slot.getItem.isEmpty && slot.getItem.getItem == item
+
   }
 
 }
