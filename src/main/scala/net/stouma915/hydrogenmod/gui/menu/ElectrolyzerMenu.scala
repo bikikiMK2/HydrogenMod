b24805c62e3df59489d608ac86162cef32543668
stouma915 <sasakitouma@yahoo.ne.jp>
2021-12-06 15:17:09 +0900

    ElectrolyzerMenu.scalaを整理

--- src/main/scala/net/stouma915/hydrogenmod/gui/menu/ElectrolyzerMenu.scala
+++ src/main/scala/net/stouma915/hydrogenmod/gui/menu/ElectrolyzerMenu.scala
@@ -2,8 +2,8 @@ package net.stouma915.hydrogenmod.gui.menu
 
 import net.minecraft.network.FriendlyByteBuf
 import net.minecraft.world.entity.player.{Inventory, Player}
-import net.minecraft.world.inventory.{AbstractContainerMenu, MenuType, Slot}
-import net.minecraft.world.item.{ItemStack, Items}
+import net.minecraft.world.inventory.{MenuType, Slot}
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.state.properties.Property
 import net.minecraftforge.items.{
   CapabilityItemHandler,
@@ -15,11 +15,9 @@ import net.stouma915.hydrogenmod.HydrogenMod
 import net.stouma915.hydrogenmod.block.ElectrolyzerBlock
 import net.stouma915.hydrogenmod.implicits.*
 import net.stouma915.hydrogenmod.item.BatteryItem
+import net.stouma915.hydrogenmod.meta.gui.menu.GUIMenu
 
-import java.util.function.Supplier
 import scala.collection.{immutable, mutable}
-import scala.util.chaining.*
-import scala.util.control.Breaks.*
 
 object ElectrolyzerMenu {
 
@@ -52,8 +50,7 @@ sealed class ElectrolyzerMenu private (
     id: Int,
     inventory: Inventory,
     extraData: FriendlyByteBuf
-) extends AbstractContainerMenu(ElectrolyzerMenu(), id)
-    with Supplier[Map[Int, Slot]] {
+) extends GUIMenu(id, inventory, extraData, ElectrolyzerMenu()) {
 
   private val customSlots = mutable.Map[Int, Slot]()
   private val blockPos = extraData.readBlockPos()
@@ -69,143 +66,46 @@ sealed class ElectrolyzerMenu private (
   if (iItemHandler.isNull)
     throw new IllegalStateException("IItemHandler is null.")
 
-  customSlots
-    .tap(
-      _.put(
-        0,
-        addSlot(new SlotItemHandler(iItemHandler, 0, 21, 18))
-      )
-    )
-    .tap(
-      _.put(
-        1,
-        addSlot(new SlotItemHandler(iItemHandler, 1, 39, 18))
-      )
-    )
-    .tap(
-      _.put(
-        2,
-        addSlot(new SlotItemHandler(iItemHandler, 2, 57, 18))
-      )
-    )
-    .tap(
-      _.put(
-        3,
-        addSlot(new SlotItemHandler(iItemHandler, 3, 21, 36))
-      )
-    )
-    .tap(
-      _.put(
-        4,
-        addSlot(new SlotItemHandler(iItemHandler, 4, 39, 36))
-      )
-    )
-    .tap(
-      _.put(
-        5,
-        addSlot(new SlotItemHandler(iItemHandler, 5, 57, 36))
-      )
-    )
-    .tap(
-      _.put(
-        6,
-        addSlot(new SlotItemHandler(iItemHandler, 6, 21, 54))
-      )
-    )
-    .tap(
-      _.put(
-        7,
-        addSlot(new SlotItemHandler(iItemHandler, 7, 39, 54))
-      )
-    )
-    .tap(
-      _.put(
-        8,
-        addSlot(new SlotItemHandler(iItemHandler, 8, 57, 54))
-      )
-    )
+  // format: off
+  private class CustomSlot(val index: Int, val x: Int, val y: Int, val mayPlace: ItemStack => Boolean)
+  private case class InputSlot(override val index: Int, override val x: Int, override val y: Int) extends CustomSlot(index, x, y,  _ => true)
+  private case class LimitedSlot(override val index: Int, override val x: Int, override val y: Int, override val mayPlace: ItemStack => Boolean) extends CustomSlot(index, x, y, mayPlace)
+  private case class OutputSlot(override val index: Int, override val x: Int, override val y: Int) extends CustomSlot(index, x, y, _ => false)
+  // format: on
 
-  customSlots.put(
-    9,
-    addSlot(new SlotItemHandler(iItemHandler, 9, 80, 18) {
+  List[CustomSlot](
+    InputSlot(0, 21, 18),
+    InputSlot(1, 39, 18),
+    InputSlot(2, 57, 18),
+    InputSlot(3, 21, 36),
+    InputSlot(4, 39, 36),
+    InputSlot(5, 57, 36),
+    InputSlot(6, 21, 54),
+    InputSlot(7, 39, 54),
+    InputSlot(8, 57, 54),
+    LimitedSlot(9, 80, 18, _.getItem == BatteryItem()),
+    OutputSlot(10, 103, 18),
+    OutputSlot(11, 121, 18),
+    OutputSlot(12, 139, 18),
+    OutputSlot(13, 103, 36),
+    OutputSlot(14, 121, 36),
+    OutputSlot(15, 139, 36),
+    OutputSlot(16, 103, 54),
+    OutputSlot(17, 121, 54),
+    OutputSlot(18, 139, 54)
+  ).foreach { customSlot =>
+    val slotItemHandler = new SlotItemHandler(
+      iItemHandler,
+      customSlot.index,
+      customSlot.x,
+      customSlot.y
+    ) {
       override def mayPlace(stack: ItemStack): Boolean =
-        stack.getItem == BatteryItem()
-    })
-  )
+        customSlot.mayPlace(stack)
+    }
 
-  customSlots
-    .tap(
-      _.put(
-        10,
-        addSlot(new SlotItemHandler(iItemHandler, 10, 103, 18) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        11,
-        addSlot(new SlotItemHandler(iItemHandler, 11, 121, 18) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        12,
-        addSlot(new SlotItemHandler(iItemHandler, 12, 139, 18) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        13,
-        addSlot(new SlotItemHandler(iItemHandler, 13, 103, 36) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        14,
-        addSlot(new SlotItemHandler(iItemHandler, 14, 121, 36) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        15,
-        addSlot(new SlotItemHandler(iItemHandler, 15, 139, 36) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        16,
-        addSlot(new SlotItemHandler(iItemHandler, 16, 103, 54) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        17,
-        addSlot(new SlotItemHandler(iItemHandler, 17, 121, 54) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
-    .tap(
-      _.put(
-        18,
-        addSlot(new SlotItemHandler(iItemHandler, 18, 139, 54) {
-          override def mayPlace(stack: ItemStack): Boolean = false
-        })
-      )
-    )
+    customSlots.put(customSlot.index, addSlot(slotItemHandler))
+  }
 
   (0 to 2).foreach { a =>
     (0 to 8).foreach { b =>
@@ -216,143 +116,6 @@ sealed class ElectrolyzerMenu private (
   }
   (0 to 8).foreach { a =>
     addSlot(new Slot(inventory, a, 0 + 8 + a * 18, 0 + 142))
-  }
-
-  override def quickMoveStack(p_38941_ : Player, p_38942_ : Int): ItemStack = {
-    var itemStack = ItemStack.EMPTY
-    val slot = slots.get(p_38942_)
-
-    if (slot.nonNull && slot.hasItem) {
-      val itemStack1 = slot.getItem
-      itemStack = itemStack1.copy()
-
-      if (p_38942_ < 0) {
-        if (
-          !moveItemStackTo(
-            itemStack1,
-            0,
-            slots.size(),
-            p_38907_ = true
-          )
-        )
-          return ItemStack.EMPTY
-
-        slot.onQuickCraft(itemStack1, itemStack)
-      } else if (!moveItemStackTo(itemStack1, 0, 0, p_38907_ = false)) {
-        if (p_38942_ < 0 + 27) {
-          if (
-            !moveItemStackTo(
-              itemStack1,
-              0 + 27,
-              slots.size(),
-              p_38907_ = true
-            )
-          )
-            return ItemStack.EMPTY
-        } else {
-          if (!moveItemStackTo(itemStack1, 0, 0 + 27, p_38907_ = false))
-            return ItemStack.EMPTY
-        }
-      }
-
-      if (itemStack1.getCount == 0)
-        slot.set(ItemStack.EMPTY)
-      else
-        slot.setChanged()
-
-      if (itemStack1.getCount == itemStack.getCount)
-        return ItemStack.EMPTY
-
-      slot.onTake(p_38941_, itemStack1)
-    }
-
-    itemStack
-  }
-
-  override def moveItemStackTo(
-      p_38904_ : ItemStack,
-      p_38905_ : Int,
-      p_38906_ : Int,
-      p_38907_ : Boolean
-  ): Boolean = {
-    var canMove = false
-    var i = if (p_38907_) p_38906_ - 1 else p_38905_
-
-    if (p_38904_.isStackable)
-      breakable {
-        while (!p_38904_.isEmpty) {
-          if (p_38907_ && i < p_38905_)
-            break()
-          else if (i >= p_38906_)
-            break()
-
-          val slot = slots.get(i)
-          val itemStack = slot.getItem
-
-          if (
-            slot.mayPlace(itemStack) && itemStack.isEmpty && ItemStack
-              .isSameItemSameTags(p_38904_, itemStack)
-          ) {
-            val j = itemStack.getCount + p_38904_.getCount
-            val maxSize =
-              math.min(slot.getMaxStackSize, p_38904_.getMaxStackSize)
-
-            if (j <= maxSize) {
-              p_38904_.setCount(0)
-              itemStack.setCount(j)
-              slot.set(itemStack)
-              canMove = true
-            } else if (itemStack.getCount < maxSize) {
-              p_38904_.shrink(maxSize - itemStack.getCount)
-              itemStack.setCount(maxSize)
-              slot.set(itemStack)
-              canMove = true
-            }
-          }
-
-          if (p_38907_)
-            i -= 1
-          else
-            i += 1
-        }
-      }
-
-    if (!p_38904_.isEmpty) {
-      if (p_38907_)
-        i = p_38906_ - 1
-      else
-        i = p_38905_
-
-      breakable {
-        while (true) {
-          if (p_38907_ && i < p_38905_)
-            break()
-          else if (i >= p_38906_)
-            break()
-
-          val slot = slots.get(i)
-          val itemStack = slot.getItem
-
-          if (itemStack.isEmpty && slot.mayPlace(p_38904_)) {
-            if (p_38904_.getCount > slot.getMaxStackSize)
-              slot.set(p_38904_.split(slot.getMaxStackSize))
-            else
-              slot.set(p_38904_.split(p_38904_.getCount))
-
-            slot.setChanged()
-            canMove = true
-            break()
-          }
-
-          if (p_38907_)
-            i -= 1
-          else
-            i += 1
-        }
-      }
-    }
-
-    canMove
   }
 
   override def stillValid(p_38874_ : Player): Boolean = true
---
+++ src/main/scala/net/stouma915/hydrogenmod/meta/gui/menu/GUIMenu.scala
@@ -0,0 +1,158 @@
+package net.stouma915.hydrogenmod.meta.gui.menu
+
+import net.minecraft.network.FriendlyByteBuf
+import net.minecraft.world.entity.player.{Inventory, Player}
+import net.minecraft.world.inventory.{AbstractContainerMenu, MenuType, Slot}
+import net.minecraft.world.item.ItemStack
+import net.stouma915.hydrogenmod.implicits.*
+
+import java.util.function.Supplier
+import scala.collection.mutable
+import scala.util.control.Breaks.*
+
+abstract class GUIMenu(
+    id: Int,
+    inventory: Inventory,
+    extraData: FriendlyByteBuf,
+    menuType: MenuType[_]
+) extends AbstractContainerMenu(menuType, id)
+    with Supplier[Map[Int, Slot]] {
+
+  override def quickMoveStack(p_38941_ : Player, p_38942_ : Int): ItemStack = {
+    var itemStack = ItemStack.EMPTY
+    val slot = slots.get(p_38942_)
+
+    if (slot.nonNull && slot.hasItem) {
+      val itemStack1 = slot.getItem
+      itemStack = itemStack1.copy()
+
+      if (p_38942_ < 0) {
+        if (
+          !moveItemStackTo(
+            itemStack1,
+            0,
+            slots.size(),
+            p_38907_ = true
+          )
+        )
+          return ItemStack.EMPTY
+
+        slot.onQuickCraft(itemStack1, itemStack)
+      } else if (!moveItemStackTo(itemStack1, 0, 0, p_38907_ = false)) {
+        if (p_38942_ < 0 + 27) {
+          if (
+            !moveItemStackTo(
+              itemStack1,
+              0 + 27,
+              slots.size(),
+              p_38907_ = true
+            )
+          )
+            return ItemStack.EMPTY
+        } else {
+          if (!moveItemStackTo(itemStack1, 0, 0 + 27, p_38907_ = false))
+            return ItemStack.EMPTY
+        }
+      }
+
+      if (itemStack1.getCount == 0)
+        slot.set(ItemStack.EMPTY)
+      else
+        slot.setChanged()
+
+      if (itemStack1.getCount == itemStack.getCount)
+        return ItemStack.EMPTY
+
+      slot.onTake(p_38941_, itemStack1)
+    }
+
+    itemStack
+  }
+
+  override def moveItemStackTo(
+      p_38904_ : ItemStack,
+      p_38905_ : Int,
+      p_38906_ : Int,
+      p_38907_ : Boolean
+  ): Boolean = {
+    var canMove = false
+    var i = if (p_38907_) p_38906_ - 1 else p_38905_
+
+    if (p_38904_.isStackable)
+      breakable {
+        while (!p_38904_.isEmpty) {
+          if (p_38907_ && i < p_38905_)
+            break()
+          else if (i >= p_38906_)
+            break()
+
+          val slot = slots.get(i)
+          val itemStack = slot.getItem
+
+          if (
+            slot.mayPlace(itemStack) && itemStack.isEmpty && ItemStack
+              .isSameItemSameTags(p_38904_, itemStack)
+          ) {
+            val j = itemStack.getCount + p_38904_.getCount
+            val maxSize =
+              math.min(slot.getMaxStackSize, p_38904_.getMaxStackSize)
+
+            if (j <= maxSize) {
+              p_38904_.setCount(0)
+              itemStack.setCount(j)
+              slot.set(itemStack)
+              canMove = true
+            } else if (itemStack.getCount < maxSize) {
+              p_38904_.shrink(maxSize - itemStack.getCount)
+              itemStack.setCount(maxSize)
+              slot.set(itemStack)
+              canMove = true
+            }
+          }
+
+          if (p_38907_)
+            i -= 1
+          else
+            i += 1
+        }
+      }
+
+    if (!p_38904_.isEmpty) {
+      if (p_38907_)
+        i = p_38906_ - 1
+      else
+        i = p_38905_
+
+      breakable {
+        while (true) {
+          if (p_38907_ && i < p_38905_)
+            break()
+          else if (i >= p_38906_)
+            break()
+
+          val slot = slots.get(i)
+          val itemStack = slot.getItem
+
+          if (itemStack.isEmpty && slot.mayPlace(p_38904_)) {
+            if (p_38904_.getCount > slot.getMaxStackSize)
+              slot.set(p_38904_.split(slot.getMaxStackSize))
+            else
+              slot.set(p_38904_.split(p_38904_.getCount))
+
+            slot.setChanged()
+            canMove = true
+            break()
+          }
+
+          if (p_38907_)
+            i -= 1
+          else
+            i += 1
+        }
+      }
+    }
+
+    canMove
+  }
+
+}
